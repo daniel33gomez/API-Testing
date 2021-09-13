80294fdbaabe72a87f00d1ff3f058cdd40ac70b0
Daniel Gomez Betancur <daniel33.gomez@gmail.com>
2021-09-13 10:41:26 -0500

    add assertion in API tests

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -1,24 +1,31 @@
 require('dotenv').config()
+const assert = require('chai').assert;
 const request = require('supertest')('https://www.wrike.com/api/v4');
 
-describe('GET /account', function() {
-    it('responds with json', function(done) {
+describe('GET /account', () => {
+    it('responds with json', () => {
       request
         .get('/account')
         .set('Authorization', `Bearer ${process.env.WRIKE_TOKEN}`)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        .expect(200, done);
+        .expect(200)
+        .then(res => {
+            assert.isNotEmpty(res.body);
+        });
     });
 });
 
-describe('GET /folders', function() {
-    it('responds with json', function(done) {
+describe('GET /folders', () => {
+    it('responds with json', () => {
       request
         .get('/account')
         .set('Authorization', `Bearer ${process.env.WRIKE_TOKEN}`)
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        .expect(200, done);
+        .expect(200)
+        .then(res => {
+            assert.isNotEmpty(res.body);
+        })
     });
 });
